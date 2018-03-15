272a5bfa0fed4872094aaa8aebd1234aa46038f1
whiletruedothis <rodrigo.torricodelcastillo@gmail.com>
2018-03-15 03:39:20 -0400

    added draw of turtle

--- Ray-Tracer/VariousObjects.cpp
+++ Ray-Tracer/VariousObjects.cpp
@@ -4,7 +4,6 @@ VariousObjects::VariousObjects(): Tracer() {}
 
 VariousObjects::VariousObjects(World* _worldPtr): Tracer(_worldPtr) {}
 
-
 VariousObjects::~VariousObjects() {}
 
 ColorRGB VariousObjects::trace_ray(const Ray& ray)const
@@ -14,7 +13,6 @@ ColorRGB VariousObjects::trace_ray(const Ray& ray)const
 	ColorRGB colorLuz(1.0, 0.4, 0.1);
 	if (sr.impactAnObject)
 	{
-
 		sr.color.r = 0.72 * colorLuz.r * std::max(0.0, sr.normal*sr.world.lights[0]->getDirection(sr));
 		sr.color.g = 0.72 * colorLuz.g * std::max(0.0, sr.normal*sr.world.lights[0]->getDirection(sr));
 		sr.color.b = 0.72 * colorLuz.b * std::max(0.0, sr.normal*sr.world.lights[0]->getDirection(sr));
